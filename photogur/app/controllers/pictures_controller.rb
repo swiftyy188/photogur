dbe689ce285ea12f1586657360fdb0de51a66ed4
swiftyy188 <jeffho.ml@gmail.com>
2015-11-09 17:35:57 -0500

    Adding feature to edit pictures

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -19,8 +19,23 @@ class PicturesController < ApplicationController
 		else
 			render :new
 		end
-		# render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params}"
+
+	def edit
+		@picture = Picture.find(params[:id])
 	end
+
+	def update
+		@picture = Picture.find(params[:id])
+
+		if @picture.update_attributes(picture_params)
+			redirect_to "/picture/#{@picture.id}"
+		else
+			render :edit
+		end
+		
+	end
+		# render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params}"
+	
 	private
 	def picture_params
 		params.require(:picture) .permit(:artist, :title, :url)
